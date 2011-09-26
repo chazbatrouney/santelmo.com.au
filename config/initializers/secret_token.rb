9d6c291c181e613098f56d26e5de35b2053fddec
Chaz Batrouney <chaz@chaz.com.au>
2011-09-26 21:12:14 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SantelmoComAu::Application.config.secret_token = '1d8fac3a29ff81801b780bfef822f7c12fc35da4550af76668de03a0dc1d69d003dbaf0b6b199dbe13b49ccb33415d6faa9c805c30e49cb48f95fafadb60755c'
