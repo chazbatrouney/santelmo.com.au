624dc18b9bcd5d08d3a3fcac33a7347136440965
Chaz Batrouney <chaz@chaz.com.au>
2011-09-30 01:01:19 +1000

    Added new helper for heading_break line

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -4,4 +4,9 @@ module MainHelper
       "&#8226;".html_safe * 29
     end
   end
+  def heading_break
+    content_tag :span, :class => 'line_break' do
+      "&#8226;".html_safe * 151
+    end
+  end
 end
