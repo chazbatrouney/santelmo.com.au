02a3bb514d4a35dbd36792ec6eb7162923057f84
Chaz Batrouney <chaz@chaz.com.au>
2011-10-05 01:10:52 +1100

    Modified helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def nav_link(text, path)
+    link = line_break + link_to(text, path)
+    
+    if current_page? path
+      link += image_tag("cross.png", :size => "24x24", :alt => "Active Cross", :class => "active_link")
+    end
+    
+    link
+  end
+  
+  def line_break(width = 29)
+    content_tag :div, :class => 'line_break' do
+      "&#8226;".html_safe * width
+    end
+  end
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,7 +1,2 @@
 module MainHelper
-  def line_break(width = 29)
-    content_tag :span, :class => 'line_break' do
-      "&#8226;".html_safe * width
-    end
-  end
 end
