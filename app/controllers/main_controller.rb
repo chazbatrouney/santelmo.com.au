c66290a4f32e9b6c6fc9d8469fc07c220dbe9ac7
Chaz Batrouney <chaz@chaz.com.au>
2011-10-24 19:22:36 +1100

    Removed alternate background layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,41 +1,5 @@
 class MainController < ApplicationController
-  def splash
-    render :layout => "splash"
-  end
-  
-  def index
-    render :layout => "background"
-  end
-  
-  def main
-  end
-  
-  def philosophy
-    render :layout => "background"
-  end
 
-  def menu
-    render :layout => "background"
-  end
-
-  def drinks
-    render :layout => "background"
-  end
-
-  def reservations
-    render :layout => "background"
-  end
-
-  def gallery
-  end
-
-  def press
-  end
-
-  def contact
-    render :layout => "background"
-  end
-  
   N_IMAGES_PER_PAGE = 6
   def gallery
     params[:page] ||= 1
@@ -46,8 +10,8 @@ class MainController < ApplicationController
       {:title => "Image Three", :url => 'image-3.jpg', :thumbnail_url => 'thumbnail-3.jpg'},
       {:title => "Image Four", :url => 'image-4.jpg', :thumbnail_url => 'thumbnail-4.jpg'},
       {:title => "Image Five", :url => 'image-5.jpg', :thumbnail_url => 'thumbnail-5.jpg'},
-      {:title => "Image Six", :url => 'image-6.jpg', :thumbnail_url => 'thumbnail-6.jpg'}
-#       {:title => "Image Seven", :url => 'image-2.jpg', :thumbnail_url => 'thumbnail-7.jpg'},
+      {:title => "Image Six", :url => 'image-6.jpg', :thumbnail_url => 'thumbnail-6.jpg'},
+#       {:title => "Image Seven", :url => 'image-1.jpg', :thumbnail_url => 'thumbnail-1.jpg'}
 #       {:title => "Image Eight", :url => 'image-2.jpg', :thumbnail_url => 'thumbnail-8.jpg'},
 #       {:title => "Image Nine", :url => 'image-2.jpg', :thumbnail_url => 'thumbnail-9.jpg'},
 #       {:title => "Image Ten", :url => 'image-2.jpg', :thumbnail_url => 'thumbnail-10.jpg'},
