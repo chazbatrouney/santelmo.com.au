0ca38b978215159332a6299bcb564b4598812ea2
Chaz Batrouney <chaz@chaz.com.au>
2011-10-24 19:36:57 +1100

    Added query to slide in #main div

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,13 @@ $(function() {
 
 
 
+$('#bg').live('pjax:start', function() {
+  $('#main').hide('slide', {direction: 'right'})
+});
+
 $('#bg').live('pjax:end', function(event, xhr, options) {
   $('.link').removeClass('current')
     .filter(function() { return $(this).find('a').get(0).href == options.url; }).addClass('current');
+
+  $('#main').show('slide', {direction: 'right'})
 });
