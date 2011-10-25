9d1f9fb8d8f1331fedf97a00b35db8bf8ea11bf6
Tom Coleman <tom@thesnail.org>
2011-10-25 16:14:57 +1100

    Fixed problem where gallery wasn't set if you load it outside of pjax
    Hacky JS solution

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,15 @@
-$(function() {
+function initPage() {
   $('a.lightbox').lightBox(); // Select all links with lightbox class
+  if (window.location.href.match(/gallery/)) {
+    $('body').addClass('gallery');
+  } else {
+    $('body').removeClass('gallery');
+  }  
+}
+
+$(function() {
   $('a.pjax').pjax('#bg', {fragment: '#bg', timeout: 2000});
+  initPage();
 });
 
 $('#bg').live('pjax:start', function() {
@@ -10,13 +19,7 @@ $('#bg').live('pjax:start', function() {
 $('#bg').live('pjax:end', function(event, xhr, options) {
   $('.link').removeClass('current')
     .filter(function() { return $(this).find('a').get(0).href == options.url; }).addClass('current');
-  $('a.lightbox').lightBox(); // Select all links with lightbox class
   $('#main').show('slide', {direction: 'right'})
   
-  if (options.url.match(/gallery/)) {
-  $('body').addClass('gallery');
-  } else {
-  $('body').removeClass('gallery');
-}
-  
+  initPage();
 });
