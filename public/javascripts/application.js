408de6e26e94a4a5e066e2ed938970468fc1e7fe
Chaz Batrouney <chaz@chaz.com.au>
2011-10-25 16:10:11 +1100

    Added JS to add class to body

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,6 +10,13 @@ $('#bg').live('pjax:start', function() {
 $('#bg').live('pjax:end', function(event, xhr, options) {
   $('.link').removeClass('current')
     .filter(function() { return $(this).find('a').get(0).href == options.url; }).addClass('current');
-
+  $('a.lightbox').lightBox(); // Select all links with lightbox class
   $('#main').show('slide', {direction: 'right'})
+  
+  if (options.url.match(/gallery/)) {
+  $('body').addClass('gallery');
+  } else {
+  $('body').removeClass('gallery');
+}
+  
 });
