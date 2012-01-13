3929ee27f33cf17b072a2343d4f2a9c1b95564ca
Chaz Batrouney <chaz@chaz.com.au>
2012-01-13 22:16:31 +1100

    Perform our own caching that sets the expiry to 24 hours

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # cache all pages for 24 hours
+  after_filter :cache_action 
+  
+  def cache_action
+    return unless perform_caching
+    
+    response.headers['Cache-Control'] = 'public, max-age=86400'
+    response.headers.delete('cookie')
+  end
 end
