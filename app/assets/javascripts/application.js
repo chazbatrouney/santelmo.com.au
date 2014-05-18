fcaaa253157adeefec467143bb702e382cc736fe
Chaz Batrouney <chaz@chaz.com.au>
2014-05-18 18:54:41 +1000

    Modified twitter feed to use twitter API Conflicts: 	Gemfile.lock 	app/assets/stylesheets/application.css.scss 	app/views/layouts/_menu.html.erb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,42 +21,6 @@ function initPage() {
   } else {
     $('body').removeClass('gallery');
   }
-  
-  // init any twitter guys (code stolen from BindleMe)
-  var $tweets = $('#tweets');
-  if ($tweets.length) {
-    twitterlib.status("san_telmo_", { page: 1, limit: 1 }, function(data) {
-      var tweet = data[0],
-        text = twitterlib.ify.clean(twitterlib.expandLinks(data[0])),
-        link = '<p class="date_stamp"><a href="http://twitter.com/' + tweet.user.screen_name +
-          '<p>/status/' + tweet.id_str + '" class="twitter_link"><i>- ' +
-          twitterlib.time.relative(tweet.created_at) + '</i></a></p>';
-
-      $tweets.html('<p>' + text +'</p>' + link);
-    });    
-  }
-
-
-  var $events = $('#events');
-  if ($events.length) {
-    twitterlib.timeline("santelmoupdates", { page: 1, limit: 4 }, function(data) {
-      var html = ''
-      for (var i = 0; i < data.length; i++) {
-        var tweet = data[i],
-          text = twitterlib.ify.clean(twitterlib.expandLinks(tweet)),
-          link = '<p class="date_stamp"><a href="http://twitter.com/' + tweet.user.screen_name +
-            '/status/' + tweet.id_str + '" class="twitter_link"><i>- ' +
-            twitterlib.time.relative(tweet.created_at) + '</i></a></p>';
-        
-        html += '<p>' + text +'</p>' + link;
-      }
-      
-      $events.html(html);
-    });
-  }
-
-
-
 }
 
 // url equality doesn't consider parameters. Right for us now, could be wrong in future.
---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,7 @@
+require 'twitter'
+  $twitter_client = Twitter::REST::Client.new do |config|
+    config.consumer_key         = "LdPdjqQZ4BkW4ZkHOXFs78riz"
+    config.consumer_secret      = "U9ePJ3oj3WQLAv0713mimorNGA9AWpYxDvvKG5VTpppvMOu5S4"
+    config.access_token         = "337783906-xDg8hOUUzgo6hda4y54xPIvWdGuA9fWaIgkqr6Rl"
+    config.access_token_secret  = "7BaAd22QKOqUoU5fcV5ZDxoAJUnMNcbWqBPfGN6K194Yh"
+  end
